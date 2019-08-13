a3a66ac882126b34a126a56ecf4d58a54683da37
damianrincon <damian.rincon@hyperiondg.com>
2019-08-13 03:47:22 -0500

    step-5

--- lib/src/calculator.dart
+++ lib/src/calculator.dart
@@ -10,10 +10,51 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+  String values, result;
+  Color background = Color(0xFFF5F7F9);
 
   @override
   void initState() {
+    values = "0";
+    result = "";
     super.initState();
+  }
+
+  void setVal(String val){
+    setState(() {
+      if (values == "0")
+        values = val;
+      else
+        values+= val;
+    });
+  }
+
+  void clear(){
+    setState(() {
+      values = "0";
+      result = "";
+    });
+  }
+
+  void delete(){
+    setState(() {
+      values = (values.length>1 && values != "0")  ? values.substring(0,values.length-1) : "0";
+    });
+  }
+
+  void calculate(){
+    try {
+      Parser p = new Parser();
+      ContextModel cm = new ContextModel();
+      Expression exp = p.parse(values);
+      setState(() {
+        result = exp.evaluate(EvaluationType.REAL, cm).toString();
+      });
+    } catch (e) {
+      setState(() {
+        result = "Sintaxis Error";
+      });
+    }
   }
 
   @override
